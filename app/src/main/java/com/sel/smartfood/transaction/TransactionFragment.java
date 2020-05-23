2732f5bc442ee256795c36dcf6a55fe1621a2d7c
kalinaxl <huynhminhtitan@gmail.com>
2020-05-23 22:24:34 +0700

    edit ui of transaction

--- app/src/main/java/com/sel/smartfood/transaction/TransactionFragment.java
+++ app/src/main/java/com/sel/smartfood/transaction/TransactionFragment.java
@@ -1,13 +1,21 @@
 package com.sel.smartfood.transaction;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.navigation.Navigator;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.sel.smartfood.R;
 
 /**
@@ -15,6 +23,8 @@ import com.sel.smartfood.R;
  */
 public class TransactionFragment extends Fragment {
 
+    private CardView withdrawCv;
+    private CardView depositCv;
     public TransactionFragment() {
         // Required empty public constructor
     }
@@ -26,5 +36,22 @@ public class TransactionFragment extends Fragment {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_transaction, container, false);
         return inflater.inflate(R.layout.fragment_transaction, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        withdrawCv = view.findViewById(R.id.include_withdraw);
+        depositCv = view.findViewById(R.id.include_deposit);
+    }
+    public void displayBottomSheet(View view){
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext(), R.style.Theme_Design_BottomSheetDialog);
+        View bottomSheetView = LayoutInflater.from(getContext()).inflate(R.layout.item_payment_service, null);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
+
+    private void navigateToPaymentSrviceChoice(View v){
+        Navigation.findNavController(v).navigate(R.id.action_nav_transaction_to_choosePaymentServiceFragment);
     }
 }
