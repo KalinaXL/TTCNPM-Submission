58c1d730e71e57f1c747d5ed82242e67440418cb
kalinaxl <huynhminhtitan@gmail.com>
2020-05-24 13:22:20 +0700

    add transaction, to be continued

--- app/src/main/java/com/sel/smartfood/transaction/TransactionFragment.java
+++ app/src/main/java/com/sel/smartfood/transaction/TransactionFragment.java
@@ -10,13 +10,17 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.navigation.Navigator;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.sel.smartfood.R;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,10 +29,7 @@ public class TransactionFragment extends Fragment {
 
     private CardView withdrawCv;
     private CardView depositCv;
-    public TransactionFragment() {
-        // Required empty public constructor
-    }
-
+    private BottomSheetDialog bottomSheetDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,17 +42,23 @@ public class TransactionFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        bottomSheetDialog = new BottomSheetDialog(requireContext());
         withdrawCv = view.findViewById(R.id.include_withdraw);
         depositCv = view.findViewById(R.id.include_deposit);
+        withdrawCv.setOnClickListener(this::displayBottomSheet);
+        depositCv.setOnClickListener(this::displayBottomSheet);
     }
     public void displayBottomSheet(View view){
-        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext(), R.style.Theme_Design_BottomSheetDialog);
-        View bottomSheetView = LayoutInflater.from(getContext()).inflate(R.layout.item_payment_service, null);
+        View bottomSheetView = LayoutInflater.from(requireContext()).inflate(R.layout.layout_choose_payment_type, null, false);
+        bottomSheetView.findViewById(R.id.ll_online_payment_type).setOnClickListener(this::navigateToPaymentServiceChoice);
+        bottomSheetView.findViewById(R.id.ll_payment_account_type).setOnClickListener(v->{
+            Toast.makeText(requireContext(), "Payment account", Toast.LENGTH_SHORT).show();
+        });
         bottomSheetDialog.setContentView(bottomSheetView);
         bottomSheetDialog.show();
     }
-
-    private void navigateToPaymentSrviceChoice(View v){
-        Navigation.findNavController(v).navigate(R.id.action_nav_transaction_to_choosePaymentServiceFragment);
+    private void navigateToPaymentServiceChoice(View v){
+        NavHostFragment.findNavController(this).navigate(R.id.action_nav_transaction_to_choosePaymentServiceFragment);
+        bottomSheetDialog.dismiss();
     }
 }
