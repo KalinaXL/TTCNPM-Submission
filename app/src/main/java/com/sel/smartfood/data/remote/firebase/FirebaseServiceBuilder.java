ed7156d570748cbd8f8f243aa81500bf15887371
kalinaxl <huynhminhtitan@gmail.com>
2020-06-09 11:42:58 +0700

    fix bug when refactor

---
+++ app/src/main/java/com/sel/smartfood/data/remote/firebase/FirebaseServiceBuilder.java
@@ -0,0 +1,32 @@
+package com.sel.smartfood.data.remote.firebase;
+
+public class FirebaseServiceBuilder implements FirebaseService.Builder{
+    private FirebaseAuthentication firebaseAuth;
+    private FirebasePaymentAccount firebasePaymentAccount;
+    private FirebaseRegistration firebaseRegistration;
+
+    @Override
+    public FirebaseService.Builder addAuth(FirebaseAuthentication auth) {
+        this.firebaseAuth = auth;
+        return this;
+    }
+
+    @Override
+    public FirebaseService.Builder addPaymentAccount(FirebasePaymentAccount paymentAccount) {
+        this.firebasePaymentAccount = paymentAccount;
+        return this;
+    }
+
+    @Override
+    public FirebaseService.Builder addRegistration(FirebaseRegistration firebaseRegistration) {
+        this.firebaseRegistration = firebaseRegistration;
+        return this;
+    }
+
+    @Override
+    public FirebaseService build() {
+        return new FirebaseService(firebaseAuth,
+                                   firebasePaymentAccount,
+                                   firebaseRegistration);
+    }
+}
--- app/src/main/java/com/sel/smartfood/login/data/remote/firebase/FirebasePaymentAccount.java
+++
@@ -1,8 +0,0 @@
-package com.sel.smartfood.login.data.remote.firebase;
-
-import androidx.annotation.Nullable;
-
-public interface FirebasePaymentAccount {
-    @Nullable
-    Float getBalance(String uuid);
-}
